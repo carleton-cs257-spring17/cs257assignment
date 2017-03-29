45c4adacb6bb943396e58e2fb655d16b584af0a6
YanhanLyu <lyuy@carleton.edu>
2017-03-29 18:47:29 -0500

    Added the final version of TTT3D IntelliJ project

--- TTT3D/src/edu/carleton/lyuy/Main.java
+++ TTT3D/src/edu/carleton/lyuy/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("JordiKai wrote this text! Yanhan wrote this text again!");
+        System.out.println("Finally, Yanhan wrote this print statement!");
     }
 }
