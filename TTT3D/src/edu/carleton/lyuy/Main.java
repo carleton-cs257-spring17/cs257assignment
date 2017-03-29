ebfa6c0aa9780aaac1b5bffc3ad33c90804dfee0
YanhanLyu <lyuy@carleton.edu>
2017-03-29 17:00:18 -0500

    Modified the TTT3D IntelliJ project again.

--- TTT3D/src/edu/carleton/lyuy/Main.java
+++ TTT3D/src/edu/carleton/lyuy/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("JordiKai wrote this text!");
+        System.out.println("JordiKai wrote this text! Yanhan wrote thie text!");
     }
 }
