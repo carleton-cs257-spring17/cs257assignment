de1eabdc39b946261cd9eba88676d93e0ef207c9
YanhanLyu <lyuy@carleton.edu>
2017-04-01 12:40:13 -0500

    Added TTT3DBoard.java, TTT3DMove.java and TTT3DMover.java;

---
+++ TTT3D/src/edu/carleton/lyuy/TTT3DMove.java
@@ -0,0 +1,25 @@
+package edu.carleton.lyuy;
+/**
+ * This class represents a single move on a 4x4x4 3D tic-tac-toe board.
+ * <p>
+ * FOR DISCUSSION:<br>
+ * What do you think about using public instance variables
+ * here instead of getters like "public int getRow()"?<br>
+ * What do you think about including the player in this class?
+ *
+ * @author Jeff Ondich
+ * @version 30 March 2017
+ */
+public class TTT3DMove {
+    public int level;
+    public int row;
+    public int column;
+    public Character player;
+
+    TTT3DMove(int level, int row, int column, Character player) {
+        this.level = level;
+        this.row = row;
+        this.column = column;
+        this.player = player;
+    }
+}
---
+++ TTT3D/src/edu/carleton/lyuy/TTT3DMoverTest.java
@@ -0,0 +1,33 @@
+package edu.carleton.lyuy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by yanhanlyu on 01/04/2017.
+ */
+class TTT3DMoverTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void winningMoves() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void blockingMoves() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void forcingMoves() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bestMove() {
+    }
+
+}
