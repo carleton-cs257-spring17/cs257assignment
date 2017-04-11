2c60af8aced2617db3d71240522e4bb25124f88a
YanhanLyu <lyuy@carleton.edu>
2017-04-11 16:07:20 -0500

    Fixed bugs in main

--- TTT3D/src/edu/carleton/lyuy/TTT3DMover.java
+++ TTT3D/src/edu/carleton/lyuy/TTT3DMover.java
@@ -570,45 +570,55 @@ public class TTT3DMover {
         return null;
     }
 
+    /**
+     * The main method of TTT3DMover. It displays the board and the place should be
+     * put 'X' or 'O' based on different actions.
+     * @param args the command arguments. The first represents action, and the second represents the board file
+     */
     public static void main(String[] args) {
         String action = args[0];
         String file = args[1];
-        TTT3DBoard board = new TTT3DBoard();
-        TTT3DMover mover = new TTT3DMover();
-        List<TTT3DMove> moveList = new ArrayList<TTT3DMove>();
-        board.loadFromFile(file);
-        if (action.equals("block")) {
-            moveList = mover.blockingMoves(board);
-            if (!moveList.isEmpty()) {
-                for (int i =0; i < moveList.size(); i++){
-                    board.setValueInSquare(moveList.get(i).level, moveList.get(i).row, moveList.get(i).column, '*');
-                }
-            }
-            board.printBoard();
-        } else if (action.equals("win")) {
-            moveList = mover.winningMoves(board);
-            if (!moveList.isEmpty()) {
-                for (int i =0; i < moveList.size(); i++){
-                    board.setValueInSquare(moveList.get(i).level, moveList.get(i).row, moveList.get(i).column, '*');
-                }
-            }
-            board.printBoard();
-        } else if (action.equals("force")) {
-            moveList = mover.forcingMoves(board);
-            if (!moveList.isEmpty()) {
-                for (int i =0; i < moveList.size(); i++){
-                    board.setValueInSquare(moveList.get(i).level, moveList.get(i).row, moveList.get(i).column, '*');
-                }
-            }
-            board.printBoard();
-        }else if (action.equals("best")) {
-            moveList.add(mover.bestMove(board));
-            if (!moveList.isEmpty()) {
-                board.setValueInSquare(moveList.get(0).level, moveList.get(0).row, moveList.get(0).column, '*');
-            }
-            board.printBoard();
+        if (args.length != 2){
+            System.out.println("Illegal input. Please input the action you want to take and the the name of board file.");
         } else {
-            System.out.print("Invalid action. No board printed");
+            TTT3DBoard board = new TTT3DBoard();
+            TTT3DMover mover = new TTT3DMover();
+            List<TTT3DMove> moveList = new ArrayList<TTT3DMove>();
+            board.loadFromFile(file);
+            if (action.equals("block")) {
+                moveList = mover.blockingMoves(board);
+                if (!moveList.isEmpty()) {
+                    for (int i =0; i < moveList.size(); i++){
+                        board.setValueInSquare(moveList.get(i).level, moveList.get(i).row, moveList.get(i).column, '*');
+                    }
+                }
+                board.printBoard();
+            } else if (action.equals("win")) {
+                moveList = mover.winningMoves(board);
+                if (!moveList.isEmpty()) {
+                    for (int i =0; i < moveList.size(); i++){
+                        board.setValueInSquare(moveList.get(i).level, moveList.get(i).row, moveList.get(i).column, '*');
+                    }
+                }
+                board.printBoard();
+            } else if (action.equals("force")) {
+                moveList = mover.forcingMoves(board);
+                if (!moveList.isEmpty()) {
+                    for (int i =0; i < moveList.size(); i++){
+                        board.setValueInSquare(moveList.get(i).level, moveList.get(i).row, moveList.get(i).column, '*');
+                    }
+                }
+                board.printBoard();
+            }else if (action.equals("best")) {
+                moveList.add(mover.bestMove(board));
+                if (!moveList.isEmpty()) {
+                    board.setValueInSquare(moveList.get(0).level, moveList.get(0).row, moveList.get(0).column, '*');
+                }
+                board.printBoard();
+            } else {
+                System.out.print("Invalid action. No board printed");
+            }
+
         }
     }
 }
