098b0432dec08586b7482bd3a343c5d147af5c7c
Lucy Wu <wul2@carleton.edu>
2017-05-03 23:53:21 -0500

    final changes

--- WebApp/test/api_test.py
+++ WebApp/test/api_test.py
@@ -5,14 +5,15 @@ Writing unittests for testing API query. We have 5 parts of tests for 5 queries 
 '''
 from unittest import TestCase
 import unittest
-import eventCalender
 import urllib.request
 import json
 
+ini_url = 'http://thacker.mathcs.carleton.edu:5223'
+
 class TestEventCalender(TestCase):
-    ini_url = 'http://thacker.mathcs.carleton.edu:5223'
+    
     def setUp(self):
-        self.event_calendar = eventCalender.EventCalender()
+        pass
 
     def tearDown(self):
         pass
@@ -43,13 +44,13 @@ class TestEventCalender(TestCase):
 
     # Test if all the events are in the department specified
     def test_department_event_department(self):
-        url = ini_url + '/events/department/Music/'
+        url = ini_url + '/events/department/Geology/'
         json_text = urllib.request.urlopen(url).read()
         json_text = json_text.decode('utf-8')
         event_list = json.loads(json_text)
         for event in event_list:
             # assert that the event's department is Music
-            self.assertEqual(event["department", "Music"])
+            self.assertEqual(event["department"], "Geology")
 
 
     # Test 2 on if events/date/<date>/ would return
@@ -84,7 +85,7 @@ class TestEventCalender(TestCase):
         event_list = json.loads(json_text)
         for event in event_list:
             # assert that the event's date is 2017-05-25
-            self.assertEqual(event["date_time"].split("T")[0], "20170525")
+            self.assertEqual(event["date_time"].split("&")[0], "20170525")
 
     # Test 3 on if events/department/date/<department>/<date>/ would return
     # a list of all events of the date and department specified
@@ -95,7 +96,7 @@ class TestEventCalender(TestCase):
 
     # Test if the returned tyoe is a list
     def test_date_dep_results_type(self):
-        url = ini_url + '/events/department/date/ComputerScience/20170525/'
+        url = ini_url + '/events/date/department/20170525/ComputerScience/'
         json_text = urllib.request.urlopen(url).read()
         json_text = json_text.decode('utf-8')
         python_object = json.loads(json_text)
@@ -105,7 +106,7 @@ class TestEventCalender(TestCase):
     # Test if the first item in the list is a dictionary
 
     def test_date_dep_event_type(self):
-        url = ini_url + '//events/department/date/ComputerScience/20170525/'
+        url = ini_url + '/events/date/department/20170525/ComputerScience/'
         json_text = urllib.request.urlopen(url).read()
         json_text = json_text.decode('utf-8')
         python_object = json.loads(json_text)
@@ -113,14 +114,14 @@ class TestEventCalender(TestCase):
 
     # Test if all the events are in the date and department specified
     def test_date_dep_event_date_dep(self):
-        url = ini_url + '/events/department/date/ComputerScience/20170525/'
-        json_text = urllib.request.open(url).read()
+        url = ini_url + '/events/date/department/20170525/ComputerScience/'
+        json_text = urllib.request.urlopen(url).read()
         json_text = json_text.decode('utf-8')
         event_list = json.loads(json_text)
         for event in event_list:
             # assert that the event's date is 2017-05-25 and department is Computer Science
-            self.assertEqual((event["date_time"].split("T")[0], event["department"]),
-                                 ('20170525', "Computer Science"))
+            self.assertEqual((event["date_time"].split("&")[0], event["department"]),
+                                 ('20170525', "ComputerScience"))
 
 
     # Test 4 on if events/all would return
@@ -132,7 +133,7 @@ class TestEventCalender(TestCase):
     # Test if the returned type is a list
 
     def test_event_sort_date_results_type(self):
-        url = ini_url + '/events/all'
+        url = ini_url + '/events/'
         json_text = urllib.request.urlopen(url).read()
         json_text = json_text.decode('utf-8')
         python_object = json.loads(json_text)
@@ -142,7 +143,7 @@ class TestEventCalender(TestCase):
     # Test if the first item in the list is a dictionary
 
     def test_event_sort_date_type(self):
-        url = ini_url + '/events/all'
+        url = ini_url + '/events/'
         json_text = urllib.request.urlopen(url).read()
         json_text = json_text.decode('utf-8')
         python_object = json.loads(json_text)
@@ -151,17 +152,16 @@ class TestEventCalender(TestCase):
     # Test if the events are sorted by date
 
     def test_event_sort_date_correct(self):
-        url = ini_url + '/events/all'
+        url = ini_url + '/events/'
         json_text = urllib.request.urlopen(url).read()
         json_text = json_text.decode('utf-8')
         event_list = json.loads(json_text)
         date_list_to_be_sorted = []
         date_list_returned = []
         for event in event_list:
-            date_parsed = ['date']
+            date_parsed = event["date_time"].split("&")[0]
             date_list_to_be_sorted.append(date_parsed)
             date_list_returned.append(date_parsed)
-        date_list_to_be_sorted.sort()
         self.assertEqual(date_list_returned, date_list_to_be_sorted)
 
 
