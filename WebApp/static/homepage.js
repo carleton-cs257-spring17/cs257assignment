2969b61ccd1e1a83dd81ecaf597e1285e544f490
YanhanLyu <lyuy@carleton.edu>
2017-05-09 19:31:21 -0500

    updated homepage

---
+++ WebApp/static/homepage.js
@@ -0,0 +1,24 @@
+/*
+ *  books-config.js
+ *  Jeff Ondich, 25 April 2017
+ *
+ *  Configuration file for the books & authors web application
+ *  for Carleton CS257, Spring Term 2017.
+ */
+
+// Set this to match the base URL at which your books_api.py is listening.
+var checkvalue[]; 
+var inputString; 
+function onAuthorsButton() {
+    checkedValue = null; 
+    var count = 0;
+    var inputElements = document.getElementsByClassName('departmentCheckbox');
+    for(var i=0; inputElements[i]; ++i){
+          if(inputElements[i].checked){
+               checkedValue[count] = inputElements[i].value;
+               count++;
+          }
+    }
+    inputString =  document.getElementById('input');
+}
+
