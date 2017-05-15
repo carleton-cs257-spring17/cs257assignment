32b3a96a25585c402a17945cf373b3e2638039ed
YanhanLyu <lyuy@carleton.edu>
2017-05-15 03:54:30 -0500

    fix most of the bugs. Only left css problems

--- WebApp/static/searchpage.js
+++ WebApp/static/searchpage.js
@@ -3,7 +3,7 @@
  *  Lucy Wu, 25 April 2017
  *
  */
-
+var body = "";
   function makeEventList(){
     var keyword = localStorage.getItem("input");
     var date = localStorage.getItem("date");
@@ -19,8 +19,8 @@
             xmlHttpRequest = new XMLHttpRequest();
             xmlHttpRequest.open('get', url);
             xmlHttpRequest.onreadystatechange = function() {
-                if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) { 
-                    callback(xmlHttpRequest.responseText);
+                if (this.readyState == 4 && this.status == 200) { 
+                    callback(this.responseText);
                 }
             }; 
             xmlHttpRequest.send(null);
@@ -45,26 +45,27 @@
             xmlHttpRequest = new XMLHttpRequest();
             xmlHttpRequest.open('get', url);
             xmlHttpRequest.onreadystatechange = function() {
-                if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) { 
-                    callback(xmlHttpRequest.responseText);
-                } 
+                if (this.readyState == 4 && this.status == 200) { 
+                    callback(this.responseText);
+                }
             }; 
+            xmlHttpRequest.send(null);
         }
-        xmlHttpRequest.send(null);
+        
     }else if (keyword.length == 0 && date.length != 0 && departmentLength != 0){
         console.log("4");
         for (var index = 0; index < departmentLength; index++){
-        url = api_base_url + "date/department/"+date+"/"+localStorage.getItem(index);
-        console.log(url);
-        xmlHttpRequest = new XMLHttpRequest();
-        xmlHttpRequest.open('get', url);
-        xmlHttpRequest.onreadystatechange = function() {
-            if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) { 
-                callback(xmlHttpRequest.responseText);
-            } 
-        }; 
+            url = api_base_url + "date/department/"+date+"/"+localStorage.getItem(index);
+            console.log(url);
+            xmlHttpRequest = new XMLHttpRequest();
+            xmlHttpRequest.open('get', url);
+            xmlHttpRequest.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) { 
+                    callback(this.responseText);
+                } 
+            }; 
+            xmlHttpRequest.send(null);
         }
-        xmlHttpRequest.send(null);
     } else if (keyword.length != 0 && date.length == 0 && departmentLength == 0){
         console.log("5");
         url = api_base_url + "keyword/"+keyword;
@@ -91,6 +92,7 @@
         xmlHttpRequest.send(null);
     } else if (keyword.length == 0 && date.length == 0 && departmentLength != 0){
         console.log("7");
+        //var xhr = [];
         //console.log("departmentLength");
         for (var index = 0; index < departmentLength; index++){
             url = api_base_url + "department/"+localStorage.getItem(index);
@@ -98,12 +100,13 @@
             xmlHttpRequest = new XMLHttpRequest();
             xmlHttpRequest.open('get', url);
             xmlHttpRequest.onreadystatechange = function() {
-                if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) { 
-                    callback(xmlHttpRequest.responseText);
+                if (this.readyState == 4 && this.status == 200) { 
+                    callback(this.responseText);
                 } 
             }; 
             xmlHttpRequest.send(null);
         }
+        
     } else {
         console.log("8");
         alert("Please give us some information")
@@ -114,12 +117,13 @@
 function callback(responseText){
     console.log(responseText);
     var eventList = JSON.parse(responseText);
-    var body = '';
-    for (var k = 0; k < eventList.length; k++) {
-        body += "<input class = 'eventCheckbox' type='checkbox'" + "value = '"+eventList[k]['name']+"'>";
-        body += "<span>"+eventList[k]['name']+"</span>"
-    }
     var resultsListElement = document.getElementById('eventsBox');
+    console.log(resultsListElement);
+    var body = resultsListElement.innerHTML;
+    for (var k = 0; k < eventList.length; k++) {
+        body += "<input class = 'eventCheckbox' type='checkbox' style= 'display: block'" + " value = '"+eventList[k]['name']+"'>";
+        body += "<label style='color:black'>"+eventList[k]['department']+": "+eventList[k]['name']+" location: "+eventList[k]['location']+ " time: "+eventList[k]['date_time']+"</label>"
+    }
     resultsListElement.innerHTML = body;
     console.log(body);
 
@@ -132,8 +136,8 @@ function onEventsButtonWishList(){
     for(var i=0; inputElements[i]; ++i){
         if(inputElements[i].checked){
             console.log("here");
-            body += "<input class = 'eventCheckbox' type='checkbox'" + "value = '"+inputElements[i].value+"'>";
-            body += "<span>"+eventList[k]['name']+"</span>"
+            body += "<input class = 'eventCheckbox' type='checkbox' style= 'display: table'" + "value = '"+inputElements[i].value+"'>";
+            body += "<span style='color:black'>"+inputElements[i].value+"</span>"
         }
     }
     var resultsListElement = document.getElementById('listBox');
