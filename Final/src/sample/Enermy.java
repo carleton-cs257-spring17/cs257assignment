9992ce6381127249f6144f01eeb6e50bb7325d66
YanhanLyu <lyuy@carleton.edu>
2017-05-29 09:26:36 -0500

    update

---
+++ Final/src/sample/Enermy.java
@@ -0,0 +1,22 @@
+package sample;
+
+import java.util.ArrayList;
+
+/**
+ * Created by yanhanlyu on 28/05/2017.
+ */
+public class Enermy{
+    int getDensity(){
+        return 1;
+    }
+    void setDensity(int density){}
+    int getSpeed(){
+        return 1;
+    }
+    void setSpeed(int speed){}
+    ArrayList getPlants(){
+        return null;
+    }
+    void addPlants(Zombie zombie){}
+
+}
--- Final/src/sample/Player.java
+++ Final/src/sample/Player.java
@@ -1,7 +1,24 @@
 package sample;
 
+import java.util.ArrayList;
+
 /**
  * Created by yanhanlyu on 28/05/2017.
  */
 public class Player {
+    int getSun(){
+        return 0;
+    }
+    ArrayList getPlants(){
+        return null;
+    }
+    void setScore(int score){
+
+    }
+    void setSun(int money){
+
+    }
+    void addPlants(Plant plant){
+
+    }
 }
