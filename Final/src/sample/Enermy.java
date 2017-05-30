78f8817b075b07b7ce8154f2419f3749a8e9e0af
Lucy Wu <wul2@carleton.edu>
2017-05-30 00:10:17 -0500

    More comment

--- Final/src/sample/Enermy.java
+++ Final/src/sample/Enermy.java
@@ -10,21 +10,30 @@ public class Enermy{
     private int density;
     private int speed;
     private ArrayList<Zombie> zombies;
+
+    // Get the density of attacking enemy
     public int getDensity(){
         return density;
     }
+    // Set the density of attacking enemy
     public void setDensity(int density){
         this.density = density;
     }
+
+    // Get the speed of attacking enemy
     public int getSpeed(){
         return speed;
     }
+    // Set the speed of attacking enemy
     public void setSpeed(int speed){
         this.speed = speed;
     }
+
+    // Get the list of all Zombies attacking
     public ArrayList<Zombie> getZombies(){
         return zombies;
     }
+    // Set the list of all Zombies attacking
     public void addZombies(Zombie zombie){
         zombies.add(zombie);
     }
--- Final/src/sample/Plant.java
+++ Final/src/sample/Plant.java
@@ -5,12 +5,23 @@ import java.util.ArrayList;
  * Interface for the characters of Plant
  */
 public interface Plant {
+    // Set the position of a plant given coordinates
     public void setPosition(int x, int y);
+    // Get the position of a plant
     public int[] getPosition();
+
+    // Get the health status of a plant
     public int getHealth();
+    // Set the health status of a plant
     public void setHealth(int health);
+
+    // Get the power value of a plant
     public int getPower();
+    // Set the power value of a plant
     public void setPower(int power);
+
+    // Get the price of a plant
     public int getPrice();
+    // Set the price of a plant
     public void setPrice(int price);
 }
