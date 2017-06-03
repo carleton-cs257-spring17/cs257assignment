9414d9e2453cf5ff259ef07211506360a0619192
YanhanLyu <lyuy@carleton.edu>
2017-06-03 17:18:19 -0500

    done

--- Final/src/sample/Controller.java
+++ Final/src/sample/Controller.java
@@ -51,10 +51,6 @@ public class Controller implements EventHandler<KeyEvent> {
 
     }
 
-    public boolean getWin(){
-        return win;
-    }
-
     /**
      * run starterTimer
      */
@@ -82,65 +78,41 @@ public class Controller implements EventHandler<KeyEvent> {
      * Update animation in this method
      */
     private void updateAnimation() {
-        //System.out.println(this.player.getSun());
-        ArrayList<Plant> listOfPlants = checkPlants();
-        ArrayList<Zombie> listOfZombies = checkZombies();
-        ArrayList<Pea> listOfPeas = checkPeas();
-        // System.out.println(listOfPlants.size());
-<<<<<<< HEAD
-        try{
-            for (Iterator<Pea> iterator3 = player.getPeas().iterator(); iterator3.hasNext(); ) {
-                Pea pea = iterator3.next();
-                if (pea.getImagePositionX()>1000){
-                    player.getPeas().remove(pea);
+            //System.out.println(this.player.getSun());
+            ArrayList<Plant> listOfPlants = checkPlants();
+            ArrayList<Zombie> listOfZombies = checkZombies();
+            ArrayList<Pea> listOfPeas = checkPeas();
+            // System.out.println(listOfPlants.size());
+
+            try {
+                for (Iterator<Pea> iterator3 = player.getPeas().iterator(); iterator3.hasNext(); ) {
+                    Pea pea = iterator3.next();
+                    if (pea.getImagePositionX() > 1000) {
+                        player.getPeas().remove(pea);
+                    }
+                    pea.step();
                 }
-                pea.step();
-=======
 
-        // Update pea animation and remove the peas that are out of the screen
-        for (Iterator<Pea> iterator3 = player.getPeas().iterator(); iterator3.hasNext(); ) {
-            Pea pea = iterator3.next();
-            if (pea.getImagePositionX()>1000){
-                player.getPeas().remove(pea);
->>>>>>> 39af6dcce2491fbeab076a6a2c3915fcb6820074
+            } catch(ConcurrentModificationException e){
+
             }
-        } catch (ConcurrentModificationException e){
+
+            // Update plant animation
+            for (Plant plant : listOfPlants) {
+                plant.step();
+            }
+            // Update Zombie animation
+            for (Zombie zombie : enermy.getZombies()) {
+                zombie.step();
+            }
+            listOfPlants = checkPlants();
+            listOfZombies = checkZombies();
+            listOfPeas = checkPeas();
+            runFight(listOfPlants, listOfZombies, listOfPeas);
 
         }
 
-<<<<<<< HEAD
 
-=======
-        // Update plant animation
->>>>>>> 39af6dcce2491fbeab076a6a2c3915fcb6820074
-        for (Plant plant : listOfPlants){
-            plant.step();
-        }
-
-        // Update Zombie animation
-        for (Zombie zombie : enermy.getZombies()){
-            zombie.step();
-        }
-
-        listOfPlants = checkPlants();
-        listOfZombies = checkZombies();
-        listOfPeas = checkPeas();
-
-        runFight(listOfPlants, listOfZombies, listOfPeas);
-
-
-//
-//        for (Zombie zombie : listOfZombies){
-//            zombie.step();
-//        }
-//
-////        for (Iterator<Plant> iterator = player.getPlants().iterator(); iterator.hasNext();){
-////            Plant plant = iterator.next();
-////            plant.step();
-////        }
-    }
-
-    // Get list of plants
     private ArrayList<Plant> checkPlants(){
         ArrayList<Plant> listOfPlants = player.getPlants();
         return listOfPlants;
@@ -220,51 +192,18 @@ public class Controller implements EventHandler<KeyEvent> {
                         }
                     }
                 }
-
-//                System.out.println("ZOMBIE row: "+zombieRow);
-//                System.out.println("ZOMBIE col: "+zombieCol);
-
-
-<<<<<<< HEAD
                 if (plantRow == zombieRow && plantColumn == zombieCol) {
-=======
-//                System.out.println("PLANT ROW: " + plantRow);
-//                System.out.println("PLANT COL: " + plantColumn);
-//                System.out.println("ZOMBIE ROW: " + zombieRow);
-//                System.out.println("ZOMBIE COL: " + zombieCol);
 
-                // If zombie arrives at a block where a plant is located
-                // zombie starts to consume plant's health value
-                if (plantRow == zombieRow && plantColumn == zombieCol) {
-//                    System.out.println("here!!");
-
-                    // Make the zombie stop when it meets a plant
->>>>>>> 39af6dcce2491fbeab076a6a2c3915fcb6820074
                     zombie.setSpeed(0);
                     blockZombie.add(zombie);
                     int plantHealth = plant.getHealth();
                     int zombiePower = zombie.getPower();
                     plant.setHealth(plantHealth-zombiePower);
-<<<<<<< HEAD
+
                     System.out.println(plant.getHealth());
                     if (plant.getHealth() < 50) {
                         if (plant.getHealth() <= 0){
                             plantDie = true;
-=======
-                    if (plant.getHealth() <= 0) {
-                        System.out.println("plant is dying");
-                        iterator.remove();
-                        int count = 0;
-
-                        // If the plant is consumed, zombie move again
-                        zombie.setSpeed(zombie.getISpeed());
-                        System.out.println("ZERO SPEED ZOMBIE COUNT: " + count);
-
-                        player.removePlants(plant);
-                        plant.removeImage();
-                        if (plant.getName().equals("sunflower")){
-                            plant.removeStar();
->>>>>>> 39af6dcce2491fbeab076a6a2c3915fcb6820074
                         }
                         zombie.setSpeed(zombie.getISpeed());
                     }
